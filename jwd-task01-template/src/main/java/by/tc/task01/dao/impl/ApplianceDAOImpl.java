59a938679f1f2e73aa45f60d5ff6f9876a44ef2d
StanislavKozlovsky <staskozlovski506@gmail.com>
2017-10-08 20:04:21 +0300

    added logic before creating a object

--- jwd-task01-template/src/main/java/by/tc/task01/dao/impl/ApplianceDAOImpl.java
+++ jwd-task01-template/src/main/java/by/tc/task01/dao/impl/ApplianceDAOImpl.java
@@ -4,17 +4,46 @@ import by.tc.task01.dao.ApplianceDAO;
 import by.tc.task01.entity.Appliance;
 import by.tc.task01.entity.criteria.Criteria;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 public class ApplianceDAOImpl implements ApplianceDAO{
+
+	private final static String PATH = "src\\main\\resources\\appliances_db.txt";
 
 	@Override
 	public <E> Appliance find(Criteria<E> criteria) {
-		// you may add your own code here
+		try(BufferedReader bufferedReader = new BufferedReader(new FileReader(PATH))){
+
+			String fileLine;
+			while ((fileLine = bufferedReader.readLine()) != null){
+				if(fileLine.isEmpty()){
+					continue;
+				}
+
+				String applianceName = fileLine.substring(0,fileLine.indexOf(" "));
+				if(applianceName.equals(criteria.getParamClassName())){
+
+					boolean containsCriteriaFlag =true;
+					List<String> listCriteria = criteria.getListCriteria();
+					for(String certainCriterion : listCriteria){
+						if(!fileLine.contains(certainCriterion)){
+							containsCriteriaFlag = false;
+						}
+					}
+
+					if(containsCriteriaFlag){
+						///////////
+					}
+				}
+			}
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
 		return null;
 	}
-	
-	// you may add your own code here
 
 }
-
-
-//you may add your own new classes
