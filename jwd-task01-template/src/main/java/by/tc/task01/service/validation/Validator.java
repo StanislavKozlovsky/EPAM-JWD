d4c230102c9e9999d9fc3e420220b31a7f7fe54a
StanislavKozlovsky <staskozlovski506@gmail.com>
2017-10-08 17:17:47 +0300

    added validator

--- jwd-task01-template/src/main/java/by/tc/task01/service/validation/Validator.java
+++ jwd-task01-template/src/main/java/by/tc/task01/service/validation/Validator.java
@@ -2,14 +2,35 @@ package by.tc.task01.service.validation;
 
 import by.tc.task01.entity.criteria.Criteria;
 
+import java.util.List;
+
 public class Validator {
-	
+
+	private final static String stringTypeSearchCriteria = "OS, FILTER_TYPE, BAG_TYPE, WAND_TYPE, COLOR, FREQUENCY_RANGE";
+
+	/**
+	 * Cases of failed validation:
+	 * 1.userCriteriaList is empty
+	 * 2.NumberFormatException during parsing for the values of the criteria, except those whose values can be a string
+	 */
 	public static <E> boolean criteriaValidator(Criteria<E> criteria) {
-		// you may add your own code here
-		
+		List<String> userCriteriaList = criteria.getListCriteria();
+		if (userCriteriaList.isEmpty()) {
+			return false;
+		}
+
+		for (String certainCriterion: userCriteriaList) {
+			String searchCriterion = certainCriterion.substring(0,certainCriterion.indexOf("="));
+			if(!stringTypeSearchCriteria.contains(searchCriterion)) {
+				String value = certainCriterion.substring(certainCriterion.indexOf("=")+1,certainCriterion.length());
+				try {
+					Double d = Double.parseDouble(value);
+				}catch (NumberFormatException e) {
+					return false;
+				}
+			}
+		}
 		return true;
 	}
 
 }
-
-//you may add your own new classes
